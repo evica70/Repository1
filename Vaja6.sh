ed3ec380b2b0646c80d83f5c63e6661e1370d482
evica70 <konradeva47@gmail.com>
2022-12-01 20:47:25 +0100

    Create Vaja6.sh

---
+++ Vaja6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo mkdir Downloads
+sudo mkdir Desktop
+sudo mkdir Documents
+sudo mkdir Pictures
+sudo mkdir Videos
+
+for i in {1..5 }
+do
+mkdir folder$i
+done
+
+for i in {1..5 }
+do
+sudo adduser i
+done
+
+
+
