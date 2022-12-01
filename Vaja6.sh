4e14c5919a73d874f95bbe91f34b18007204d969
evica70 <119625880+evica70@users.noreply.github.com>
2022-12-01 21:49:33 +0100

    Update Vaja6.sh

--- Vaja6.sh
+++ Vaja6.sh
@@ -10,10 +10,11 @@ do
 mkdir folder$i
 done
 
-for i in {1..5 }
-do
-sudo adduser Uporabnik$i
-done
+1 sudo adduser Uporabnik1
+2 sudo adduser Uporabnik2
+3 sudo adduser Uporabnik3
+4 sudo adduser Uporabnik4
+5 sudo adduser Uporabnik5
 
 
 
